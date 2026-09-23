5a1cb9a9d32415a9e14eb5cec9a580ea0795c0bd
helloinfinityinfra-design <hello.infinityinfra@gmail.com>
2026-09-23 16:35:49 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import BlogsPage from './components/BlogsPage'
 import BlogDetail from './components/BlogDetail'
+import LandConverterWidget from './components/LandConverterWidget'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blogs" element={<BlogsPage />} />
-      <Route path="/vlog" element={<BlogsPage />} />
-      <Route path="/vlogs" element={<BlogsPage />} />
-      <Route path="/blog/:slug" element={<BlogDetail />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<BlogsPage />} />
+        <Route path="/vlog" element={<BlogsPage />} />
+        <Route path="/vlogs" element={<BlogsPage />} />
+        <Route path="/blog/:slug" element={<BlogDetail />} />
+      </Routes>
+      <LandConverterWidget />
+    </>
   )
 }
 
 export default App
+
